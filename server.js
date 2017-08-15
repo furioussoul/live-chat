20fc06bb46d3c86ba3758135c4d71c6de45d5ddf
szj46941 <szj46941@tc.com>
2017-08-15 12:29:57 +0800

    改端口80

--- server.js
+++ server.js
@@ -228,6 +228,6 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(8080, function () {
-  console.log('listening on *:8080');
+http.listen(80, function () {
+  console.log('listening on *:80');
 });
