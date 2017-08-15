bb6ea844ec6fe94dfbf723d80782a4cfbe0ac145
szj46941 <szj46941@tc.com>
2017-08-15 13:26:28 +0800

    release0.0.1

--- server.js
+++ server.js
@@ -223,6 +223,6 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(8080, function () {
+http.listen(80, function () {
   console.log('listening on *:80');
 });
--- src/js/store.js
+++ src/js/store.js
@@ -9,8 +9,7 @@ import {ChatClient} from './client'
 Vue.use(Vuex);
 
 function connect(state) {
-  // state.client = new ChatClient({host: '47.94.2.0', port: 80})//todo js获取origin
-  state.client = new ChatClient({host: '127.0.0.1', port: 8080})//todo js获取origin
+  state.client = new ChatClient({host: '47.94.2.0', port: 80})//todo js获取origin
   if (!state.client.connect()) {
     alert('连接失败')
   }
