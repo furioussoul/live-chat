ffe894e6f70ed517a103769682a6f6696886e14a
sunzhengjie <sunzhengjie@kashuo.com>
2017-08-19 23:26:00 +0800

    实现消息通知

--- src/frontend/js/store.js
+++ src/frontend/js/store.js
@@ -16,7 +16,7 @@ import {
 Vue.use(Vuex);
 
 function connect(state) {
-  state.client = new ChatClient({host: '127.0.0.1', port: 80})
+  state.client = new ChatClient({host: '47.94.2.0', port: 80})
   if (!state.client.connect()) {
     alert('连接失败')
   }
